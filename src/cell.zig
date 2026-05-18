7e215008dae2fb70f5ebe8ac6a8efb1503d9704a
Tanmay Chaudhry <tanmay.chaudhry@gmail.com>
2026-05-18 10:19:40 +0530

    core: Cell packed struct with defaults, Grid double-buffer with diff-based rendering, alt screen, cursor hide/show, resize

--- src/cell.zig
+++ src/cell.zig
@@ -1,7 +1,7 @@
 pub const Cell = packed struct {
     char: u21, // 21 bits
-    fg: u8, // 21 + 8 = 29 bits
-    bg: u8, // 29 + 8 = 37 bits
+    fg: u8 = 7, // 21 + 8 = 29 bits
+    bg: u8 = 0, // 29 + 8 = 37 bits
     bold: bool, // 37 + 1 = 38 bits
     italic: bool, // 38 + 1 = 39 bits
     underline: bool, // 39 + 1 = 40 bits
--- src/grid.zig
+++ src/grid.zig
@@ -1,16 +1,18 @@
 const std = @import("std");
 const cell = @import("cell.zig");
-const csi = "0x1b]";
+
+const csi = "\x1b[";
 
 pub const Grid = struct {
     width: usize,
     height: usize,
     allocator: std.mem.Allocator,
+    writer: *std.Io.Writer,
     desired: []cell.Cell,
     current: []cell.Cell,
 };
 
-pub fn init(allocator: std.mem.Allocator, width: usize, height: usize) !Grid {
+pub fn init(allocator: std.mem.Allocator, writer: *std.Io.Writer, width: usize, height: usize) !Grid {
     const current: []cell.Cell = try allocator.alloc(cell.Cell, width * height);
     errdefer allocator.free(current);
     @memset(current, cell.EMPTY);
@@ -23,6 +25,7 @@ pub fn init(allocator: std.mem.Allocator, width: usize, height: usize) !Grid {
         .height = height,
         .current = current,
         .desired = desired,
+        .writer = writer,
     };
 }
 
@@ -31,16 +34,24 @@ pub fn deinit(self: *Grid) void {
     self.allocator.free(self.current);
 }
 
+pub fn start(self: *Grid) !void {
+    try self.writer.print("{s}?1049h", .{csi});
+    try self.writer.print("{s}?25l", .{csi});
+}
+
+pub fn stop(self: *Grid) !void {
+    try self.writer.print("{s}?1049l", .{csi});
+    try self.writer.print("{s}?25h", .{csi});
+}
+
 pub fn put(self: *Grid, x: usize, y: usize, c: cell.Cell) !void {
     if ((x >= self.width) or (y >= self.height)) {
         return error.CoordinatesOutOfBounds;
     }
-    self.desired[self.calcOffset(x, y)] = c;
+    self.desired[self.calcOffsetFromCoordinates(x, y)] = c;
 }
 
-pub fn flush(self: *Grid, io: std.Io, writer: *std.Io.Writer) !void {
-    _ = io;
-    _ = writer;
+pub fn flush(self: *Grid) !void {
     // Diff against current
     var diffs = try std.ArrayList(usize).initCapacity(self.allocator, (self.width * self.height));
     defer diffs.deinit();
@@ -51,18 +62,40 @@ pub fn flush(self: *Grid, io: std.Io, writer: *std.Io.Writer) !void {
             diffs.appendAssumeCapacity(i);
         }
     }
-    for (diffs) |c| {
-        getCharSequence(self.desired[c]);
+    for (diffs) |i| {
+        const coords = calcCoordinatesFromOffset(self, i);
+        writeCharJumpSequence(self.writer, coords.x, coords.y);
+        writeCharSequence(self.writer, self.desired[i]);
     }
+    try self.writer.flush();
 
     @memcpy(self.current, self.desired);
 }
 
-fn getCharSequence(c: *cell.Cell) [64]u8 {
-    var buf: [64]u8 = undefined;
-    var cur = 0;
-    @memcpy(buf[cur..2], csi);
-    cur += 2;
+pub fn resize(self: *Grid, width: usize, height: usize) !void {
+    self.deinit();
+    self.* = try init(self.allocator, self.writer, width, height);
+}
+
+inline fn writeCharJumpSequence(writer: *std.Io.Writer, x: usize, y: usize) !void {
+    try writer.print("{s}{d};{d}H", .{ csi, y + 1, x + 1 });
+}
+
+fn writeCharSequence(writer: *std.Io.Writer, c: *cell.Cell) !void {
+    // optimize this later
+    const attrs = [_]struct { name: []const u8, code: u8 }{
+        .{ .name = "bold", .code = 1 },
+        .{ .name = "italic", .code = 3 },
+        .{ .name = "underline", .code = 4 },
+        .{ .name = "reverse", .code = 7 },
+    };
+    try writer.print("{s}0;", .{csi}); // Reset
+    inline for (attrs) |attr| {
+        if (@field(c, attr.name)) {
+            try writer.print("{d};", .{attr.code});
+        }
+    }
+    try writer.print("38;5;{d};48;5;{d}m{u}", .{ c.fg, c.bg, c.char });
 }
 
 inline fn calcOffsetFromCoordinates(self: *Grid, x: usize, y: usize) usize {
